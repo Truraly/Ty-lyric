45f66fa87e2b33080910b6d4a7cae735577aaf78
Truraly <1293915326@qq.com>
2023-11-21 21:18:45 +0800

    小bug

--- .vitepress/theme/VexTab.vue
+++ .vitepress/theme/VexTab.vue
@@ -7,7 +7,7 @@ const props = defineProps(["foo"]);
 // const Renderer = Vex.Flow.Renderer;
 
 let cl = setInterval(() => {
-  // @ts-ignore
+  if (document == undefined) return clearInterval(cl);
   const element = document.getElementById("boo");
   if (element) {
     clearInterval(cl);
---
+++ moveLogo.sh
@@ -0,0 +1,5 @@
+
+
+# 如果路径不存在就创建
+mkdir -p ./dist/node_modules/vextab/dist
+cp node_modules/vextab/dist/div.dev.js dist/node_modules/vextab/dist/div.dev.js 
